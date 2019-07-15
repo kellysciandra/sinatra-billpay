9067d94011f9009cef7b21743074d7e26d7c77e9
Kelly Sciandra <kellysciandra@icloud.com>
2019-07-15 13:50:22 -0400

    project submit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, "flatiron_school"
     use Rack::Flash, :sweep => true
 
   end
